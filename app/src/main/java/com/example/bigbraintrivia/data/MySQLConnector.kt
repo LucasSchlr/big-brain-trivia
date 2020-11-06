88c4a04b2c9b3f142c7c431ba717d4af5e61f4ca
Eduardo Arndt <eduardoarndt1@gmail.com>
2020-11-05 22:01:32 -0300

    database connector class

---
+++ app/src/main/java/com/example/bigbraintrivia/data/MySQLConnector.kt
@@ -0,0 +1,20 @@
+package com.example.bigbraintrivia.data
+
+import java.sql.Connection
+import java.sql.DriverManager
+
+private const val driverName = "com.mysql.cj.jdbc.Driver"
+private const val jdbcMySQLPath = "jdbc:mysql://localhost/big-brain-trivia"
+private const val databaseUser = "admin"
+private const val databaseSecret = "admin"
+
+
+object MySQLConnector {
+    var connection: Connection
+
+    init {
+        println("[data] Stabilising connection with MySQL...")
+        Class.forName(driverName).getDeclaredConstructor().newInstance()
+        connection = DriverManager.getConnection(jdbcMySQLPath, databaseUser, databaseSecret)
+    }
+}
